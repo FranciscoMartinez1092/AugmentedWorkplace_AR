26725a80c14c3a8db5d8c66e4bca9dd5d4336b57
AugmentedWorkplace <augmentedworkplace@outlook.com>
2019-04-10 03:32:26 -0400

    Added button manager for mesh handling

---
+++ Samples/MRVC/Unity/Assets/ButtonManager.cs
@@ -0,0 +1,42 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ButtonManager : MonoBehaviour, IInputClickHandler, IInputHandler
+{
+    public GameObject SpatialMappingObject;
+    public GameObject Canvas;
+    public CustomNetworkManager networkManager;
+    private LogStates logger;
+
+    private bool Observing = true;
+    void Start ()
+    {
+        // Add debug text logger
+        logger = Canvas.GetComponent<LogStates>();
+    }
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        // AirTap code goes here
+        List<Mesh> meshes = SpatialMappingObject.GetComponent<SpatialMappingManager>().GetMeshes();
+        logger.AppendMeshDebugText(meshes.Count.ToString() + " meshes.");
+        foreach (Mesh mesh in meshes)
+        {
+            networkManager.sendSpatialMesh(mesh.vertices, mesh.uv, mesh.triangles);
+        }
+        //if (!Observing)
+        //{
+        //    SpatialMappingObject.GetComponent<SpatialMappingManager>().StartObserver();
+        //    Observing = !Observing;
+        //} else
+        //{
+        //    SpatialMappingObject.GetComponent<SpatialMappingManager>().StopObserver();
+        //    Observing = !Observing;
+        //}
+    }
+    public void OnInputDown(InputEventData eventData)
+    { }
+    public void OnInputUp(InputEventData eventData)
+    { }
+}
